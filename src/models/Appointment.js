99ecc4f9c898601aeedd3670df2e9b51c2de607b
Alexs Ruiz <ralexs.acu@gmail.com>
2022-11-25 21:55:17 -0600

    Feat(models): define appointment model

---
+++ src/models/Appointment.js
@@ -0,0 +1,33 @@
+import { model, Schema } from 'mongoose'
+
+const appointmentSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    description: String,
+    startTime: {
+      type: Date,
+      required: true
+    },
+    endTime: {
+      type: Date,
+      required: true
+    }
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at', // Use `created_at` to store the created date
+      updatedAt: 'updated_at' // and `updated_at` to store the last updated date
+    }
+  }
+)
+
+const appointment = new model('Appointment', appointmentSchema)
+export default appointment
