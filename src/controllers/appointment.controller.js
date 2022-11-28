6dd78b71ecc31d912d778ec5598f8a86bacbb974
Alexs Ruiz <ralexs.acu@gmail.com>
2022-11-27 21:15:27 -0600

    Feat(appointment): CRUD methods for Appointment controller
    - add check validations to the createNewAppointment methods

--- src/controllers/appointment.controller.js
+++ src/controllers/appointment.controller.js
@@ -1,17 +1,104 @@
 import Appointment from '../models/Appointment.js'
 
-export const getAllAppointments = (_, res) => {
-  // CODE HERE
+export const getAllAppointments = async (_, res) => {
+  const appointments = await Appointment.find().populate('user', 'name')
+
+  if (appointments.length > 0) {
+    res.status(200).json({
+      ok: true,
+      appointments
+    })
+    return
+  }
+
+  res.status(200).json({
+    ok: true,
+    message: 'Currently, there are no appointments'
+  })
 }
 
-export const createNewAppointment = (req, res) => {
-  // CODE HERE
+export const createNewAppointment = async (req, res) => {
+  try {
+    const appointment = await new Appointment(req.body)
+
+    // TODO: add the logged In user ID (createdBy)
+
+    const appointmentCreated = await appointment.save()
+
+    res.status(201).json({
+      ok: true,
+      appointmentCreated
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      message: 'Something was wrong'
+    })
+  }
 }
 
-export const updateAppointment = (req, res) => {
-  // CODE HERE
+export const updateAppointment = async (req, res) => {
+  try {
+    const appointment = await Appointment.findById(req.params.id)
+
+    if (!appointment) {
+      return res.status(404).json({
+        ok: false,
+        msg: `appointment with ID ${req.params.id} does not exist`
+      })
+    }
+
+    // TODO: VALIDATE IF THE USER HAS PERMISSIONS TO EDIT
+
+    const newAppointment = {
+      ...req.body,
+      user: req.uid
+    }
+
+    const appointmentUpdated = await Appointment.findByIdAndUpdate(
+      req.params.id,
+      newAppointment,
+      { new: true }
+    )
+
+    res.json({
+      ok: true,
+      appointmentUpdated
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Algo ha salido mal'
+    })
+  }
 }
 
-export const deleteAppointment = (req, res) => {
-  // CODE HERE
+export const deleteAppointment = async (req, res) => {
+  try {
+    const appoitment = await Appointment.findById(req.params.id)
+
+    if (!appoitment) {
+      return res.status(404).json({
+        ok: false,
+        msg: `appointment with ID ${req.params.id} does not exist`
+      })
+    }
+
+    // TODO: VALIDATE IF THE USER HAS PERMISSIONS TO DELETE
+
+    const appoitmentDeleted = await Appointment.findByIdAndDelete(req.params.id)
+
+    res.status(200).json({
+      ok: true,
+      deleted: appoitmentDeleted
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Algo ha salido mal'
+    })
+  }
 }
--- src/routes/appointments.route.js
+++ src/routes/appointments.route.js
@@ -5,11 +5,24 @@ import {
   getAllAppointments,
   updateAppointment
 } from '../controllers/appointment.controller.js'
+import { check } from 'express-validator'
+import isDate from '../helpers/isDate.js'
+import fieldValidator from '../middlewares/field-validator.js'
 
 const router = Router()
 
 router.get('/', getAllAppointments)
-router.post('/', createNewAppointment)
+router.post(
+  '/',
+  [
+    check('title', 'title is required').not().isEmpty(),
+    check('user', 'user is required').not().isEmpty(),
+    check('startTime', 'start time is required').custom(isDate),
+    check('endTime', 'end time is required').custom(isDate)
+  ],
+  fieldValidator,
+  createNewAppointment
+)
 router.put('/:id', updateAppointment)
 router.delete('/:id', deleteAppointment)
 
