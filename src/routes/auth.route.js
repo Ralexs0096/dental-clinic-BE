bef325fd18998427463bc3f661ce664f8d777233
Patrick-Cairoli <patrick.cairoli@gmail.com>
2022-12-15 16:17:05 -0600

    adds sign in route and isAuth middleware

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { check } from 'express-validator'
-import { login } from '../controllers/auth.controller.js'
 import fieldValidator from '../middlewares/field-validator.js'
+import { signUp, signIn, isAuth } from '../controllers/auth.controller.js'
+import { verifyToken } from '../middlewares/verifyToken.js'
 
 const router = Router()
 
 router.post(
-  '/',
+  '/signin',
   [
     check('email', 'Email is required').isEmail(),
     check(
@@ -15,7 +16,34 @@ router.post(
     ).isLength({ min: 6 })
   ],
   fieldValidator,
-  login
+  signIn,
+)
+
+router.post(
+  '/signup',
+  [
+    check('email', 'Email is required').isEmail(),
+    check(
+      'password',
+      'password must contain a minimum of 6 characters'
+    ).isLength({ min: 6 })
+  ],
+  fieldValidator,
+  signUp,
+)
+
+router.get(
+  '/me',
+  verifyToken,
+  [
+    check('email', 'Email is required').isEmail(),
+    check(
+      'password',
+      'password must contain a minimum of 6 characters'
+    ).isLength({ min: 6 })
+  ],
+  fieldValidator,
+  isAuth,
 )
 
 export default router
