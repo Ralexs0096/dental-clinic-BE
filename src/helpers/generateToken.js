79bc993ddf44e6a071e5d059c933c9daa3a222e2
Alexs Ruiz <ralexs.acu@gmail.com>
2023-01-18 00:40:04 -0600

    Fix(JWT): add the uid and name to the payload

--- src/helpers/generateToken.js
+++ src/helpers/generateToken.js
@@ -2,13 +2,15 @@ import jwt from 'jsonwebtoken'
 
 /**
  * generate a new token
- * @param {string} signature for the token
+ * @param {string} uid user ID
+ * @param {string} name username
  * @returns a new token
  */
 
-export const generateToken = signature => {
-  return jwt.sign({ id: signature }, process.env.SECRET, {
+export const generateToken = (uid, name) => {
+  const payload = { uid, name }
+  return jwt.sign(payload, process.env.SECRET, {
     //tokens expiration is to be determined, set to last two hours in the meantime.
-    expiresIn: 1000 * 60 * 60 * 2
+    expiresIn: '2h'
   })
 }
