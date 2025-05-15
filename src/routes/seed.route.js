8963c8be31d5d20f54c6e0b09669c2913f439958
Alexs Ruiz <ralexs.acu@gmail.com>
2025-05-14 22:56:57 -0600

    feat(seed): implement user seeding with hashed passwords

--- src/routes/seed.route.js
+++ src/routes/seed.route.js
@@ -1,14 +1,30 @@
 import { Router } from 'express'
+import bcrypt from 'bcrypt'
+import { usersData } from '../seed-data/users.js'
+import User from '../models/User.js'
 
 const router = Router()
 
-router.get('/users', (req, res) => {
-  res.send({
-    message: 'Seed executed successfully'
-  })
+router.get('/users', async (_, res) => {
+  try {
+    const usersToInsert = await Promise.all(
+      usersData.map(async user => {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(user.password, salt)
+        return { ...user, password: hashedPassword }
+      })
+    )
+
+    await User.insertMany(usersToInsert)
+
+    res.status(201).send({ message: 'Users seeded successfully' })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({ message: 'Error seeding users' })
+  }
 })
 
-router.get('/appointments', (req, res) => {
+router.get('/appointments', (_, res) => {
   res.send({
     message: 'Seed executed successfully'
   })
