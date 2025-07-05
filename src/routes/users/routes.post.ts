eef5d822d5a33c69570316893adf3a9d3b800b49
Alexs Ruiz <ralexs.acu@gmail.com>
2025-07-05 12:39:18 -0600

    Add new post route definition file for user routes

---
+++ src/routes/users/routes.post.ts
@@ -0,0 +1,24 @@
+import type { FastifyPluginAsync } from 'fastify'
+import { createUserSchema } from '../../schemas/users/request'
+import { createUserHandler } from '../../handlers/users/createUser.handler'
+import z from 'zod'
+
+const postUserRoute: FastifyPluginAsync = async fastify => {
+  fastify.post(
+    '/',
+    {
+      schema: {
+        body: createUserSchema,
+        response: {
+          201: z.object({
+            id: z.number(),
+            name: z.string()
+          })
+        }
+      }
+    },
+    createUserHandler
+  )
+}
+
+export default postUserRoute
