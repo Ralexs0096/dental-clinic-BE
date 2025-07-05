1df286f64d15c91130bd71b53c88561472a37bac
Alexs Ruiz <ralexs.acu@gmail.com>
2025-07-05 12:09:10 -0600

    Refactor users routes to include Zod schema validation for GET and POST responses

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,20 +1,51 @@
 import { FastifyPluginAsync } from 'fastify'
+import { createUserSchema, CreateUserInput } from 'schemas/users/request'
+import z from 'zod'
 
 const usersRoutes: FastifyPluginAsync = async fastify => {
-  fastify.get('/', async () => {
-    return [
-      {
-        id: 1,
-        name: 'Steve Vai'
+  fastify.get(
+    '/',
+    {
+      schema: {
+        response: {
+          201: z.object({
+            id: z.number(),
+            name: z.string()
+          })
+        }
       }
-    ]
-  })
+    },
+    async () => {
+      return [
+        {
+          id: 1,
+          name: 'Steve Vai'
+        }
+      ]
+    }
+  )
 
-  fastify.post('/', async (req, reply) => {
-    const user = req.body
+  fastify.post<{
+    Body: CreateUserInput
+  }>(
+    '/',
+    {
+      schema: {
+        body: createUserSchema,
+        response: {
+          201: z.object({
+            id: z.number(),
+            name: z.string()
+          })
+        }
+      }
+    },
+    async (req, reply) => {
+      const user = req.body
 
-    reply.code(201).send(user)
-  })
+      reply.code(201).send(user)
+    }
+  )
 }
 
 export default usersRoutes
