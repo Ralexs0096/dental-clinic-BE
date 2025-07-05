78163234e19b9a89acbe4b400ec0943035f800c7
Alexs Ruiz <ralexs.acu@gmail.com>
2025-07-05 11:25:43 -0600

    Update server port configuration to use environment variable

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
+import { env } from 'config/env'
 import CreateServer from './server'
 
 const server = CreateServer()
 
-server.listen({ port: 8080 }, err => {
+server.listen({ port: env.PORT }, err => {
   if (err) {
     console.log('Something went wrong', err)
   }
 
-  console.log('Server is running on port 8080')
+  console.log(`Server is running on port ${env.PORT}`)
 })
