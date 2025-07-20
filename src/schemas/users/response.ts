70afe83329e59f58e19d1395da75e6c1cd6903e9
Alexs Ruiz <ralexs.acu@gmail.com>
2025-07-20 17:22:57 -0600

    Create a new Schema user response

--- src/schemas/users/response.ts
+++ src/schemas/users/response.ts
@@ -1,6 +1,13 @@
 import { z } from 'zod'
 
-export const createdUserSchema = z.object({
-  id: z.number(),
-  name: z.string()
+export const userSchema = z.object({
+  id: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string(),
+  role: z.string(),
+  address: z.string().nullable().optional(),
+  phone: z.number().nullable().optional()
 })
+
+export type UserBody = z.infer<typeof userSchema>
