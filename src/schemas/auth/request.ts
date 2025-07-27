edc6dda4a97b9d7c5714a757f27abf369deeb21b
Alexs Ruiz <ralexs.acu@gmail.com>
2025-07-27 12:28:48 -0600

    Define SignIn schema and type

--- src/schemas/auth/request.ts
+++ src/schemas/auth/request.ts
@@ -3,9 +3,22 @@ import { userSchema } from '../users/response'
 
 const userWithoutId = userSchema.omit({ id: true, role: true })
 
+/**
+ * **************** /signup ****************
+ */
 export const signUpSchemaBody = z.object({
   ...userWithoutId.shape,
   password: z.string().min(8)
 })
 
 export type SignUpBody = z.infer<typeof signUpSchemaBody>
+
+/**
+ * **************** /signin ****************
+ */
+export const signInSchemaBody = z.object({
+  email: z.string(),
+  password: z.string().min(8)
+})
+
+export type SignInBody = z.infer<typeof signInSchemaBody>
