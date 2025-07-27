faf7c5e58d19df3161a77d74a641bddb283f6cd0
Alexs Ruiz <ralexs.acu@gmail.com>
2025-07-27 12:53:05 -0600

    Implement SignIn handler logic

--- src/handlers/auth/signin.handler.ts
+++ src/handlers/auth/signin.handler.ts
@@ -1,12 +1,53 @@
 import { RouteHandler } from 'fastify'
 import { PostSignInRoute } from '@/routes/auth/routes.post'
+import { PrismaClient } from '@prisma'
+import { compare } from 'bcrypt-ts'
+
+const messageError = 'Email or Password is incorrect'
 
 export const signinHandler: RouteHandler<PostSignInRoute> = async (
   req,
   reply
 ) => {
   try {
-    reply.code(200).send(req.body)
+    const prisma = new PrismaClient()
+    const { email, password } = req.body
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    })
+
+    if (!existingUser?.id) {
+      return reply.code(401).send({
+        ok: false,
+        message: messageError
+      })
+    }
+
+    const isPasswordCorrect = await compare(password, existingUser.password)
+
+    if (!isPasswordCorrect) {
+      return reply.code(401).send({
+        ok: false,
+        message: messageError
+      })
+    }
+
+    const token = req.server.generateToken(existingUser.id)
+
+    reply.code(202).send({
+      ok: true,
+      token,
+      user: {
+        id: existingUser.id,
+        email: existingUser.email,
+        firstName: existingUser.firstName,
+        lastName: existingUser.lastName,
+        role: existingUser.role
+      }
+    })
   } catch (error) {
     console.error(error)
 
