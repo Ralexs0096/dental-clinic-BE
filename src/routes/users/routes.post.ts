9852be451ae504e5e1bf3ab3023f1dcb48c32d1a
Alexs Ruiz <ralexs.acu@gmail.com>
2025-07-27 14:10:37 -0600

    Adjust create user route including the createUserHandler

--- src/routes/users/routes.post.ts
+++ src/routes/users/routes.post.ts
@@ -1,20 +1,17 @@
 import type { FastifyPluginAsync } from 'fastify'
-import { createUserSchema } from '@/schemas/users/request'
+import { CreateUserBody, createUserSchema } from '@/schemas/users/request'
 import { createUserHandler } from '@/handlers/users/createUser.handler'
-import z from 'zod'
+
+export type PostUserRoute = {
+  Body: CreateUserBody
+}
 
 const postUserRoutes: FastifyPluginAsync = async fastify => {
-  fastify.post(
+  fastify.post<PostUserRoute>(
     '/',
     {
       schema: {
-        body: createUserSchema,
-        response: {
-          201: z.object({
-            id: z.number(),
-            name: z.string()
-          })
-        }
+        body: createUserSchema
       }
     },
     createUserHandler
